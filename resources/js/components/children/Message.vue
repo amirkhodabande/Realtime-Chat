03f4210cac648f2d5fc396b564d5c7858b030cca
Amir Khodabande <amirdp8102@gmail.com>
2020-12-17 18:34:08 +0330

    Message Component created.

---
+++ resources/js/components/children/Message.vue
@@ -0,0 +1,31 @@
+<template>
+      <div>
+            <li
+                  class="list-group-item "
+                  :class="className"
+            >
+                  <slot></slot>
+            </li>
+            <small
+                  class="badge float-right"
+                  :class="badgeClassName"
+            >You</small>
+      </div>
+</template>
+
+<script>
+export default {
+      props: ["message", "color"],
+      computed: {
+            className() {
+                  return "list-group-item-" + this.color;
+            },
+            badgeClassName() {
+                  return "badge-" + this.color;
+            },
+      },
+};
+</script>
+
+<style>
+</style>
