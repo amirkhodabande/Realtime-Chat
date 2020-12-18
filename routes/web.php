a45b4d1efd82753f2c4c4334f936408e7927e060
Amir Khodabande <amirdp8102@gmail.com>
2020-12-18 14:10:50 +0330

    ...

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('/chat', 'ChatController');
+Route::apiResource('/chat', 'ChatController');
 
 Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
